4240715e298f6499bf8c127f1118d93ed8e2db56
Sai Sathwik <reddysaisathwik022@gmail.com>
2024-06-30 10:31:13 +0530

    Context API project

---
+++ 08-miniContext/src/components/Profile.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+import React, { useContext } from "react";
+import UserContext from "../context/UserContext";
+
+function Profile() {
+  const { user } = useContext(UserContext);
+
+  if (!user) return <div>please login</div>;
+
+  return <div>Welcome {user.username}</div>;
+}
+
+export default Profile;
